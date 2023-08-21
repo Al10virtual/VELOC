fc15016f1a51fbb3eef42c3e1a6e153555dfca4e
Arnaud Legaignoux <arnaud.legaignoux@gmail.com>
2023-08-21 18:16:26 +0200

    adding containers to devise views

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,6 @@
 puts "Cleaning database..."
 Bike.destroy_all
 
-
-
 puts "Creating bikes"
 
 un = {user: User.first, bike_type: "vtc", wheel_size: "700cc", frame_material: "acier", brand: "Danone", condition: "mint", price_per_day: 11.5, price_per_week: 70 }
@@ -11,7 +9,6 @@ deux =  {user: User.second, bike_type: "route", wheel_size: "29 pouces", frame_m
 trois = {user: User.first, bike_type: "cross", wheel_size: "26 pouces", frame_material: "alu", brand: "Apple", condition: "mint", price_per_day: 11.5, price_per_week: 70 }
 quatre = {user: User.second, bike_type: "bmx", wheel_size: "12 pouces", frame_material: "acier", brand: "Giant", condition: "OK", price_per_day: 22.5, price_per_week: 110 }
 cinq = {user: User.second, bike_type: "bmx", wheel_size: "14 pouces", frame_material: "carbon", brand: "Giant", condition: "OK", price_per_day: 18, price_per_week: 110 }
-
 
 [un , deux, trois, quatre, cinq].each do |attributes|
   bike = Bike.create!(attributes)
